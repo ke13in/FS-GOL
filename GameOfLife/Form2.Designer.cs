cbb3aa647fa2a81a2e8a556084d6b43dfef67ca8
Kevin Szanto <ksszanto@fullsail.edu>
2016-05-21 21:10:27 -0400

    Completed coding for form2 functionality

--- GameOfLife/Form2.Designer.cs
+++ GameOfLife/Form2.Designer.cs
@@ -30,23 +30,31 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
             this.milliUpDown = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.xUpDown = new System.Windows.Forms.NumericUpDown();
-            this.yUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label4 = new System.Windows.Forms.Label();
+            this.yUpDown = new System.Windows.Forms.NumericUpDown();
+            this.xUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.liveCellColor = new System.Windows.Forms.TextBox();
+            this.tenGlColor = new System.Windows.Forms.TextBox();
+            this.glColor = new System.Windows.Forms.TextBox();
             this.colorLabel = new System.Windows.Forms.Label();
+            this.backgroundColor = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.milliUpDown)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.xUpDown)).BeginInit();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.yUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xUpDown)).BeginInit();
             this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -73,6 +81,22 @@
             this.tabPage1.Text = "Time";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // milliUpDown
+            // 
+            this.milliUpDown.Location = new System.Drawing.Point(12, 69);
+            this.milliUpDown.Name = "milliUpDown";
+            this.milliUpDown.Size = new System.Drawing.Size(120, 22);
+            this.milliUpDown.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Milliseconds Between Generations:";
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.label4);
@@ -88,30 +112,28 @@
             this.tabPage2.Text = "Size";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // label4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 39);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Milliseconds Between Generations:";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Grid Size";
             // 
-            // milliUpDown
+            // yUpDown
             // 
-            this.milliUpDown.Location = new System.Drawing.Point(12, 69);
-            this.milliUpDown.Name = "milliUpDown";
-            this.milliUpDown.Size = new System.Drawing.Size(120, 22);
-            this.milliUpDown.TabIndex = 1;
+            this.yUpDown.Location = new System.Drawing.Point(34, 78);
+            this.yUpDown.Name = "yUpDown";
+            this.yUpDown.Size = new System.Drawing.Size(120, 22);
+            this.yUpDown.TabIndex = 3;
             // 
-            // label2
+            // xUpDown
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 17);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "X";
+            this.xUpDown.Location = new System.Drawing.Point(34, 50);
+            this.xUpDown.Name = "xUpDown";
+            this.xUpDown.Size = new System.Drawing.Size(120, 22);
+            this.xUpDown.TabIndex = 2;
             // 
             // label3
             // 
@@ -122,33 +144,25 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "Y";
             // 
-            // xUpDown
+            // label2
             // 
-            this.xUpDown.Location = new System.Drawing.Point(34, 50);
-            this.xUpDown.Name = "xUpDown";
-            this.xUpDown.Size = new System.Drawing.Size(120, 22);
-            this.xUpDown.TabIndex = 2;
-            // 
-            // yUpDown
-            // 
-            this.yUpDown.Location = new System.Drawing.Point(34, 78);
-            this.yUpDown.Name = "yUpDown";
-            this.yUpDown.Size = new System.Drawing.Size(120, 22);
-            this.yUpDown.TabIndex = 3;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(31, 21);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 17);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Grid Size";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X";
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.liveCellColor);
+            this.tabPage3.Controls.Add(this.tenGlColor);
+            this.tabPage3.Controls.Add(this.glColor);
             this.tabPage3.Controls.Add(this.colorLabel);
-            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.backgroundColor);
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Size = new System.Drawing.Size(459, 287);
@@ -156,38 +170,115 @@
             this.tabPage3.Text = "Color";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // label7
             // 
-            this.textBox1.Location = new System.Drawing.Point(57, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(25, 22);
-            this.textBox1.TabIndex = 0;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Live Cell Color";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Grid Line Color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Tenth Line Grid";
+            // 
+            // liveCellColor
+            // 
+            this.liveCellColor.Location = new System.Drawing.Point(137, 135);
+            this.liveCellColor.Name = "liveCellColor";
+            this.liveCellColor.Size = new System.Drawing.Size(36, 22);
+            this.liveCellColor.TabIndex = 4;
+            this.liveCellColor.Click += new System.EventHandler(this.liveCellColor_Click);
+            // 
+            // tenGlColor
+            // 
+            this.tenGlColor.Location = new System.Drawing.Point(137, 107);
+            this.tenGlColor.Name = "tenGlColor";
+            this.tenGlColor.Size = new System.Drawing.Size(36, 22);
+            this.tenGlColor.TabIndex = 3;
+            this.tenGlColor.Click += new System.EventHandler(this.tenGlColor_Click);
+            // 
+            // glColor
+            // 
+            this.glColor.Location = new System.Drawing.Point(137, 79);
+            this.glColor.Name = "glColor";
+            this.glColor.Size = new System.Drawing.Size(36, 22);
+            this.glColor.TabIndex = 2;
+            this.glColor.Click += new System.EventHandler(this.glColor_Click);
             // 
             // colorLabel
             // 
             this.colorLabel.AutoSize = true;
             this.colorLabel.Location = new System.Drawing.Point(10, 46);
             this.colorLabel.Name = "colorLabel";
-            this.colorLabel.Size = new System.Drawing.Size(41, 17);
+            this.colorLabel.Size = new System.Drawing.Size(121, 17);
             this.colorLabel.TabIndex = 1;
-            this.colorLabel.Text = "Color";
+            this.colorLabel.Text = "Background Color";
+            // 
+            // backgroundColor
+            // 
+            this.backgroundColor.Location = new System.Drawing.Point(137, 46);
+            this.backgroundColor.Name = "backgroundColor";
+            this.backgroundColor.Size = new System.Drawing.Size(36, 22);
+            this.backgroundColor.TabIndex = 0;
+            this.backgroundColor.Click += new System.EventHandler(this.backgroundColor_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(19, 325);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(125, 324);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 316);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(467, 354);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
             this.Controls.Add(this.tabControl1);
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Form2";
             this.Text = "Form2";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.milliUpDown)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.milliUpDown)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.xUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.yUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xUpDown)).EndInit();
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
@@ -208,6 +299,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Label colorLabel;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox backgroundColor;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox liveCellColor;
+        private System.Windows.Forms.TextBox tenGlColor;
+        private System.Windows.Forms.TextBox glColor;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
--- GameOfLife/Form2.cs
+++ GameOfLife/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GameOfLife.Properties;
 
 namespace GameOfLife
 {
@@ -15,6 +16,66 @@ namespace GameOfLife
         public Form2()
         {
             InitializeComponent();
+            milliUpDown.Value = Settings.Default.time;
+            xUpDown.Value = Settings.Default.gridX;
+            yUpDown.Value = Settings.Default.gridY;
+
+            backgroundColor.BackColor = Settings.Default.panelColor;
+            glColor.BackColor = Settings.Default.gridColor;
+            tenGlColor.BackColor = Settings.Default.gridBold;
+            liveCellColor.BackColor = Settings.Default.cellColor;
+
+        }
+
+        private void backgroundColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            color.Color = backgroundColor.BackColor;
+
+            if (DialogResult.OK == color.ShowDialog())
+            {
+                backgroundColor.BackColor = color.Color;
+            }
+        }
+
+        private void glColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            color.Color = glColor.BackColor;
+
+            if (DialogResult.OK == color.ShowDialog())
+            {
+                glColor.BackColor = color.Color;
+            }
+        }
+
+        private void tenGlColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            color.Color = tenGlColor.BackColor;
+
+            if (DialogResult.OK == color.ShowDialog())
+            {
+                tenGlColor.BackColor = color.Color;
+            }
+        }
+
+        private void liveCellColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            color.Color = liveCellColor.BackColor;
+
+            if (DialogResult.OK == color.ShowDialog())
+            {
+                liveCellColor.BackColor = color.Color;
+            }
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Settings.Default.time = (int)milliUpDown.Value;
+            Settings.Default.gridX = (int)xUpDown.Value;
+            Settings.Default.gridY = (int)yUpDown.Value;
         }
     }
 }
